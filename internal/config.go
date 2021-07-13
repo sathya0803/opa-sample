91f8b5730176818afb0e3919ce80549bcc1c7803
sathya0803 <59246072+sathya0803@users.noreply.github.com>
2021-07-13 21:20:25 +0530

    Create config.go

---
+++ internal/config.go
@@ -0,0 +1,38 @@
+package internal
+
+import (
+	"flag"
+	"path/filepath"
+
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+// Gets kubeconfig from $HOME/.kube folder
+func KubeConfigFromDir() *rest.Config {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
+
+// Gets kubeconfig from service account
+func KubeConfigInCluster() *rest.Config {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
